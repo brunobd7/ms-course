149cc12695e5dc603878ef3a6e1b474009253587
Bruno Dantas <brunoodantas7@gmail.com>
2022-08-24 12:52:05 -0300

    Using RestTemplate to comunication between services on hardcode

--- hr-payroll/src/main/java/com/dantas/hrpayroll/service/PaymentService.java
+++ hr-payroll/src/main/java/com/dantas/hrpayroll/service/PaymentService.java
@@ -1,12 +1,31 @@
 package com.dantas.hrpayroll.service;
 
 import com.dantas.hrpayroll.entities.Payment;
+import com.dantas.hrpayroll.entities.Worker;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class PaymentService {
 
+    @Value("${hr-worker.host}")
+    private String workerHost;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
     public Payment getPayment(Long workerId, Integer days){
-        return new Payment("Bob", 200.0, days);
+
+        //MAPPING PATH VARIABLES USED ON REQUEST OF ENDPOINT "findById" on "workers" service
+        Map<String, String> uriVariables = new HashMap<>();
+        uriVariables.put("id", workerId.toString());
+
+        Worker worker = restTemplate.getForObject(workerHost.concat("/workers/{id}") , Worker.class, uriVariables);
+        return new Payment(worker.getName(), worker.getDailyIncome(), days);
     }
 }
