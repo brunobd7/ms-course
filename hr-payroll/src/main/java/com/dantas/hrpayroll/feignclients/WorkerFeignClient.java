72180bacce83c37e21b30d5066e28c1d07adb665
Bruno Dantas <brunoodantas7@gmail.com>
2022-08-25 22:59:43 -0300

    Feign client ovveriding restTemplate

---
+++ hr-payroll/src/main/java/com/dantas/hrpayroll/feignclients/WorkerFeignClient.java
@@ -0,0 +1,16 @@
+package com.dantas.hrpayroll.feignclients;
+
+import com.dantas.hrpayroll.entities.Worker;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Component
+@FeignClient(name = "hr-worker" , url = "localhost:8001", path = "/workers")
+public interface WorkerFeignClient {
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Worker> findById(@PathVariable Long id);
+}
--- hr-payroll/src/main/java/com/dantas/hrpayroll/service/PaymentService.java
+++ hr-payroll/src/main/java/com/dantas/hrpayroll/service/PaymentService.java
@@ -2,30 +2,20 @@ package com.dantas.hrpayroll.service;
 
 import com.dantas.hrpayroll.entities.Payment;
 import com.dantas.hrpayroll.entities.Worker;
+import com.dantas.hrpayroll.feignclients.WorkerFeignClient;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
-import java.util.HashMap;
-import java.util.Map;
 
 @Service
 public class PaymentService {
 
-    @Value("${hr-worker.host}")
-    private String workerHost;
-
     @Autowired
-    private RestTemplate restTemplate;
+    private WorkerFeignClient feignClient;
 
     public Payment getPayment(Long workerId, Integer days){
 
-        //MAPPING PATH VARIABLES USED ON REQUEST OF ENDPOINT "findById" on "workers" service
-        Map<String, String> uriVariables = new HashMap<>();
-        uriVariables.put("id", workerId.toString());
-
-        Worker worker = restTemplate.getForObject(workerHost.concat("/workers/{id}") , Worker.class, uriVariables);
+        Worker worker = feignClient.findById(workerId).getBody();
         return new Payment(worker.getName(), worker.getDailyIncome(), days);
     }
 }
