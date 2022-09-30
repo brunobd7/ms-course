1a386278270849688d3bc58c3d7f18881236ec8b
Bruno Dantas <brunoodantas7@gmail.com>
2022-09-29 23:41:17 -0300

    Created project hr-user

---
+++ hr-user/src/test/java/com/dantas/hruser/HrUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dantas.hruser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
