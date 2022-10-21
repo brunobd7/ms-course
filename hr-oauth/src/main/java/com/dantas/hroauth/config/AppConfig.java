c9cd6a7c0d42ee740d596107c4f91e71f20b7cb6
Bruno Dantas <brunoodantas7@gmail.com>
2022-10-21 10:02:11 -0300

    Configurint login with JWT part 1

---
+++ hr-oauth/src/main/java/com/dantas/hroauth/config/AppConfig.java
@@ -0,0 +1,40 @@
+package com.dantas.hroauth.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+@Configuration
+public class AppConfig {
+
+
+    /**
+     * Configuration bean to encrypt password
+     * */
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    /**
+     * Configuration bean to manage our jwt token and set configurations
+     * */
+    @Bean
+    public JwtAccessTokenConverter accessTokenConverter(){
+        JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
+        tokenConverter.setSigningKey("MY_SECRET_KEY");
+        return  tokenConverter;
+    }
+
+    /**
+     *  This bean read information inside accessToken object
+     * */
+    @Bean
+    public JwtTokenStore tokenStore(){
+        return new JwtTokenStore(accessTokenConverter());
+    }
+
+
+}
---
+++ hr-oauth/src/main/java/com/dantas/hroauth/config/AuthorizationServerConfig.java
@@ -0,0 +1,66 @@
+package com.dantas.hroauth.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.AccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+@Configuration
+@EnableAuthorizationServer
+public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Autowired
+    private AccessTokenConverter accessTokenConverter;
+
+    @Autowired
+    private JwtTokenStore jwtTokenStore;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+        security.tokenKeyAccess("permitAll()").checkTokenAccess("isAuthenticated()");
+    }
+
+    /**
+     * Configuration of authentication and authorization basement on app/client credentials and client type.
+     * <p>
+     * @apiNote  withClient -> app/client ID
+     *  <br> secret -> app/client password access
+     *  <br> scopes -> app/client permissions read, write or both
+     *  <br> authorizedGrantTypes -> grant type used by user credentials on app/client
+     *  <br> accessTokenValidity -> time to token expiration in seconds;
+     * */
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.inMemory()
+                .withClient("myappname123")
+                .secret(bCryptPasswordEncoder.encode("myappscret123"))
+                .scopes("read","write")
+                .authorizedGrantTypes("password")
+                .accessTokenValiditySeconds(86400);
+    }
+
+    /**
+     * Configuring token processing with configuration bean crated in AppConfig
+     * */
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints.authenticationManager(authenticationManager)
+                .tokenStore(jwtTokenStore)
+                .accessTokenConverter(accessTokenConverter);
+    }
+}
